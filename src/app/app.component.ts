932e77834d8865d0d7b0eb2955621ebe53bf1874
Abdullah Ourfali <80325019+aOurfali@users.noreply.github.com>
2023-01-09 15:55:22 +0100

    first version is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -12,31 +12,33 @@ export class AppComponent {
   lines = 0;
   arrayAsVector = new Array();
   columnsText = [''];
+  output = new Array();
 
-  sortString(str: string)
-  {
-    const strAsArray = str.split('')
-    //const newArr = strAsArray.map(char => {strAsArray.indexOf(char)})
-    if (str.length <= 1)
-      return true;
-    else
-      /*let mitte = str.length / 2;
-      let l -> i <= mitte – 1
-      let r -> i >= array.length – mitte – 1
-      l = merge_sort(l)
-      r = merge_sort(r)
-      return verschmelze(l,r)*/
-      //let charCount = new Array(26);
-      //charCount.fill(0);
-      return 0;
+  @Output() textChanged = new EventEmitter();
 
+  sortIndices(toSort: string[]) {
+
+    let sortWithIndices = new Array(toSort.length);
+    let justIndices = new Array(toSort.length);
+    for (var i = 0; i < toSort.length; i++) {
+      sortWithIndices[i] = [toSort[i], i];
+    }
+    sortWithIndices.sort();
+    for (var i = 0; i < toSort.length; i++) {
+      justIndices[i] = sortWithIndices[i][1];
+    }
+    return justIndices;
+  }
+
+  transpose(matrix: any[]) {
+    let [row] = matrix;
+    return row.map((value: any, column: any) => matrix.map(row => row[column]));
   }
 
   onSubmit(f: NgForm) {
     let word: string = f.value.inputWord;
     let text: string = f.value.inputText;
 
-    debugger;
     this.columns = word.length;
     if(text.length % word.length == 0) {
       this.lines = text.length/word.length;
@@ -44,12 +46,12 @@ export class AppComponent {
     else{
       this.lines = parseInt(String(text.length/word.length)) + 1;
     }
-    
+
+    this.arrayAsVector = [...new Array(this.lines)].map(el => new Array(this.columns));
+
     const textChars = text.split('');
-
-    this.arrayAsVector = [...new Array(this.columns)].map(el => new Array(this.lines));
-
     let l = 0;
+
     for (let i = 0; i < this.lines; i++) {
       for (let j = 0; j < this.columns; j++) {
         if( l < text.length)
@@ -70,50 +72,19 @@ export class AppComponent {
       }
     }
 
-    console.log(this.arrayAsVector);
-
-    this.columnsText = word.split('');
-    this.columnsText.sort();
-    console.log(this.columnsText);
-
-    /*
-    let i = 0;
-    let j = 0;
-    let l = 0;
-
+    let newMatrix = this.transpose(this.arrayAsVector);
     
-    debugger;
-    while(l < word.length) {
-      if(i < columns && j < lines) {
-        if(textChars[l] === ' ') {
-          arrayAsVector[i][j] = "_";
-          l++;
-          i++;
-        }
-        else {
-          arrayAsVector[i][j] = textChars[l];
-          l++;
-          i++;
-        }
-      }
-      else if (i >= columns && j < lines) {
-        if(textChars[l] === ' ') {
-          j++;
-          i = 0;
-          arrayAsVector[i][j] = "_";
-          i++;
-          l++;
-        }
-        else {
-          arrayAsVector[i][j] = textChars[l];
-          l++;
-          i++;
-        }
+    this.columnsText = word.split('');
+    
+    let sortedIndices = this.sortIndices(this.columnsText);
+
+    let outputMatrix = [...new Array(this.columns)].map(el => new Array(this.lines));
+
+    for (let i = 0; i < this.columns; i++) {
+      for (let j = 0; j < this.lines; j++) {
+        outputMatrix[i][j] = newMatrix[sortedIndices[i]][j];
       }
     }
-
-    let alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    const alphabet = alph.split('');*/
-    
+    this.output = this.transpose(outputMatrix);
   }
 }
